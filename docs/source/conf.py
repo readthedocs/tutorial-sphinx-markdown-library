4f055ab33d2a381b0fa0c8c276d75665bbfb9ac3
Juan Luis Cano Rodríguez <hello@juanlu.space>
2021-10-28 09:41:14 +0000

    7. Documenting code automatically

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,13 @@ extensions = [
     "sphinx.ext.duration",
     "sphinx.ext.autosectionlabel",
     "nbsphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
 ]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None)
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- lumache.py
+++ lumache.py
@@ -14,6 +14,8 @@ def get_random_ingredients(kind=None):
     """
     Return a list of random ingredients as strings.
 
+    At the moment, only a hardcoded list is implemented.
+
     :param kind: Optional "kind" of ingredients.
     :type kind: list[str] or None
     :raise lumache.InvalidKindError: If the kind is invalid.
